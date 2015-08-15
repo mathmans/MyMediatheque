59bb8da0200a782cb0f5c1d9dd39b7b3732d0e30
Mathieu Mansart <mathieumansart@gmail.com>
2015-08-15 22:12:52 +0200

    first commit

---
+++ MyMediatheque/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMediatheque.Startup))]
+namespace MyMediatheque
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
